22f718343ccbb3057eb405ecbb6b7ffd474f9c7e
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-06 14:42:36 +0530

    modified port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const products_routes = require("./routes/products");
 
--- db/connect.js
+++ db/connect.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 
 const connectDB = (uri) => {
+  console.log("Connected to mongoDb");
   return mongoose.connect(uri);
 };
 
